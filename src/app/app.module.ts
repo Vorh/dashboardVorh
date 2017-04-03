d0ae08abced1684d5140180c9c106c5c05878c7f
vorh <sorcerer12@list.ru>
2017-04-03 10:44:34 +0300

    ADD [ todoList ]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { HeaderTodoComponent } from './components/todo-list/header-todo/header-todo.component';
+import { PanelTodoComponent } from './components/todo-list/panel-todo/panel-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
-    SideBarComponent
+    SideBarComponent,
+    TodoListComponent,
+    HeaderTodoComponent,
+    PanelTodoComponent
   ],
   imports: [
     BrowserModule,
