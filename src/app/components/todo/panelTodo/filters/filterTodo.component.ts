ec0964afaabe807debd0ec3afb385b84a051e087
vorh <sorcerer12@list.ru>
2017-05-24 23:40:49 +0300

    [ ADD ]  filterTodo

---
+++ src/app/components/todo/panelTodo/filters/filterTodo.component.ts
@@ -0,0 +1,27 @@
+import {ComboBox, Item} from "../../../ui/combobox/combobox.component";
+import {Component, Input} from "@angular/core";
+import {TodoList} from "../../todoList/todoList.component";
+import {Todo} from "../../../../models/todo";
+/**
+ * Created by vorh on 5/24/17.
+ */
+
+@Component({
+  selector: "filter-todo",
+  styleUrls: ["filterTodo.component.scss"],
+  templateUrl: "filterTodo.component.html"
+})
+
+
+export class FilterTodo extends ComboBox {
+
+
+  select(item: Item) {
+
+  }
+
+
+
+
+
+}
