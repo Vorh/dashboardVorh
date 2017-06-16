581b1778a7041233a1b301f545d62fba90011e90
vorh <sorcerer12@list.ru>
2017-06-16 21:39:26 +0300

    [ ADD ]  calendar foundation

---
+++ src/app/components/calendar/calendar-layout.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+/**
+ * Created by vorh on 6/16/17.
+ */
+
+@Component({
+  selector: 'calendar-layout',
+  templateUrl: 'calendar-layout.html',
+  styleUrls: ['calendar-layout.scss']
+})
+
+export class CalendarLayout {
+
+}
